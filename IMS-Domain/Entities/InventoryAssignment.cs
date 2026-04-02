0a1c5f36d4085eaa927beb00b68a5fe4524402d3
Kashyap-ItIdol <kashyap.itidoltechnologies@gmail.com>
2026-04-02 13:21:17 +0530

    inventory entities (#11)

---
+++ IMS-Domain/Entities/InventoryAssignment.cs
@@ -0,0 +1,21 @@
+﻿namespace IMS_Domain.Entities
+{
+    public class InventoryAssignment
+    {
+        public int Id { get; set; }
+
+        public int InventoryId { get; set; }
+
+        public Inventory Inventory { get; set; }
+
+        public int AssignedTo { get; set; }
+
+        public DateTime AssignedDate { get; set; }
+
+        public DateTime? ExpectedReturnDate { get; set; }
+
+        public string Location { get; set; }
+
+        public string Table { get; set; }
+    }
+}
---
+++ IMS-Infrastructure/Data/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using IMS_Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace IMS_Infrastructure.Data.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+
+            builder.Property(c => c.Name)
+                   .IsRequired()
+                   .HasMaxLength(150);
+        }
+    }
+}
