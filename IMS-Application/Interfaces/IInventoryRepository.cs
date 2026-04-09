122f917d645a9470a53f902c4b7c45d3d393c295
yash <yashcpatel2403@gmail.com>
2026-04-09 18:08:33 +0530

    update asset entities

--- IMS-Application/Interfaces/IInventoryRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using IMS_Application.DTOs;
-
-namespace IMS_Application.Interfaces
-{
-    public interface IInventoryRepository
-    {
-        Task<int> CreateInventoryAsync(InventoryCreateDto dto);
-    }
-}
--- IMS-Application/Services/Interfaces/IInventoryService.cs
+++
@@ -1,9 +0,0 @@
-﻿using IMS_Application.DTOs;
-
-namespace IMS_Application.Services.Interfaces
-{
-    public interface IInventoryService
-    {
-        Task<int> CreateInventoryAsync(InventoryCreateDto dto);
-    }
-}
