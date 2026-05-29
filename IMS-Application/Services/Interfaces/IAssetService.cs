67d3b88e6a76fdcc5c0d06e823a8384d7b5ff25b
sonika1544 <sonikatandel0@gmail.com>
2026-05-29 18:48:31 +0530

    Assets Related Modification

--- IMS-Application/Services/Interfaces/IAssetService.cs
+++ IMS-Application/Services/Interfaces/IAssetService.cs
@@ -18,7 +18,7 @@ namespace IMS_Application.Services.Interfaces
         Task<Result<string>> DetachChildAsync(DetachChildDto dto);
         Task<Result<string>> DetachAssignmentAsync(int assetId);
         Task<Result<List<AssetListDto>>> FilterAssetsAsync(AssetFilterDto dto);
-        Task<Result<List<AssetListDto>>> SearchAssetsAsync(string searchTerm);
+        Task<Result<List<AssetOverviewDto>>> SearchAssetsAsync(string searchTerm);
         Task<Result<string>> AddOrUpdateNetworkAsync(int assetId, NetworkDetailsDto dto, int userId);
         Task<Result<byte[]>> ExportAllAssetsCsvAsync();
         Task<Result<ImportAssetsResultDto>> ImportAssetsCsvAsync(ImportAssetsRequestDto dto, int createdBy);
